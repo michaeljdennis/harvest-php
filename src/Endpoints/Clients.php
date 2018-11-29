2449b037c448c5136c67b0e937fa4acc17fd0e7f
Michael Dennis <michaeljdennis@gmail.com>
2018-11-29 11:31:58 -0800

    Add clients endpoint

---
+++ src/Endpoints/Clients.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Harvest\Endpoints;
+
+use Harvest\Models\Client;
+
+class Clients extends BaseEndpoint
+{
+    public function getPath() : string
+    {
+        return 'clients';
+    }
+
+    public function getResponseKey() : string
+    {
+        return 'clients';
+    }
+
+    public function getModelClass() : string
+    {
+        return Client::class;
+    }
+}
---
+++ src/Models/Client.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Harvest\Models;
+
+class Client extends BaseModel
+{
+    public $id;
+    public $name;
+    public $is_active;
+    public $address;
+    public $currency;
+    public $created_at;
+    public $updated_at;
+}
